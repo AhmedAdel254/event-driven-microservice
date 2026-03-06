f8b05fc603678fe2151f6cd03b563e7e216ad5bc
ahmed adel <https://github.com/AhmedAdel254>
2026-03-06 20:44:01 +0200

    changing

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 
@@ -15,6 +17,6 @@ app.use("/api", routes);
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server running on port 3000");
 });
